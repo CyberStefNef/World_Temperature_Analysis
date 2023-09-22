ccc984685e5b40b72d78267de3d6d98c571ae0df
Gaute <gaute.k.spilde@gmail.com>
2023-09-22 18:04:32 +0200

    made_parquet

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,7 +3,19 @@
 import pandas as pd
 
 
-def load_data():
+def load_data_csv():
     """ Loading data from the data directory """
 
     return pd.read_csv('data/GlobalLandTemperaturesByCity.csv', parse_dates=['dt'],  dtype={'AverageTemperatureUncertainty': float, 'City': 'category', 'Country': 'category'})
+
+
+def load_data_parquet():
+    """ Loading data from the data directory """
+    df = pd.read_parquet('data\GlobalLandTemperaturesByCity.parquet')
+    df['dt'] = df['dt'].astype('datetime64[ns]')
+    df['City'] = df['City'].astype('category')
+    df['Country'] = df['Country'].astype('category')
+
+    return df
+
+
