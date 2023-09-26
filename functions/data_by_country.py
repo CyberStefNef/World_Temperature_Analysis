b38bf6582715788b1c2300cda527771f267f32e8
Gaute <gaute.k.spilde@gmail.com>
2023-09-26 02:06:12 +0200

    arma works

--- functions/data_by_country.py
+++ functions/data_by_country.py
@@ -1,5 +1,5 @@
 
 def data_by_country(data):
-    data = data[["Country", "AverageTemperature", "dt"]
-                ].groupby(by=["Country", "dt"], as_index=False).mean()
+    data = data[["Country", "AverageTemperature", "dt", "Country_ISO"]
+                ].groupby(by=["Country", "Country_ISO", "dt"], as_index=False).mean()
     return data
